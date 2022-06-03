08ea11c69a8bcf9f7688e13a2cd66e71d42524c7
Jnrlima <41413257+Jnrlima@users.noreply.github.com>
2022-06-03 14:14:49 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,8 @@
 echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/Jnrlima/JhonnyMusic /MusicPlayer
-apt install wget -y
-apt install git curl python3-pip ffmpeg -y
-pip3 install -U pip -y
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-npm install pm2@latest -g -y
-apt install build-essential -y
-apt install -y nodejs -y
+pip3 install -r requirements.txt
+pip3 install wget
+pip3 install nodejs
 echo ">> INSTALLING REQUIREMENTS..."
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
