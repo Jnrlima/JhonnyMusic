63dc4bc162b8765992439424cab8e6042c77f51b
Jnrlima <41413257+Jnrlima@users.noreply.github.com>
2022-06-03 14:18:36 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/Jnrlima/JhonnyMusic /MusicPlayer
 pip3 install -r requirements.txt
-pip3 install wget
-pip3 install nodejs
 echo ">> INSTALLING REQUIREMENTS..."
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
+pip3 install wget
+pip3 install nodejs
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
 echo "
