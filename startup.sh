85e65d9ac73cb26a6eb9ec094a06e06fd9a24d1b
Jnrlima <41413257+Jnrlima@users.noreply.github.com>
2022-06-08 23:50:49 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,6 +6,12 @@ cd /JhonnyMusic
 pip3 install -U -r requirements.txt
 pip3 install wget
 pip3 install nodejs
+apt install git curl python3-pip ffmpeg -y
+apt install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+apt install -y nodejs
+apt install build-essential
+npm install pm2@latest -g
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
 echo "
