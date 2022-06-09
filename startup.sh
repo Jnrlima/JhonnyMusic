32f7d609646199e60a3eeeaa95f9bb943efabc09
Jnrlima <41413257+Jnrlima@users.noreply.github.com>
2022-06-09 00:07:13 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,8 @@
 echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/Jnrlima/JhonnyMusic
-pip3 install -r requirements.txt
 echo ">> INSTALLING REQUIREMENTS..."
 cd /JhonnyMusic
-pip3 install -r requirements.txt
-apt-get install nodejs
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
-nodejs -v
-apt-get install curl
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-apt-get install nodejs
+pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
 echo "
