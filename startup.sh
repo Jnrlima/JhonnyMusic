8253479c55554171705c783fc4740e3486528dd1
Jnrlima <41413257+Jnrlima@users.noreply.github.com>
2022-06-10 01:44:19 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 echo ">> FETCHING UPSTREAM..."
 git clone https://github.com/Jnrlima/JhonnyMusic
+pprint "Installing Nodejs and Npm..  "
+curl -fssL https://deb.nodesource.com/setup_17.x | sudo -E bash - &>nodelog.txt &&
+sudo apt install nodejs -y &>>nodelog.txt &&
+sudo npm i -g npm &>>nodelog.txt &&
+pprint "SUCCESS!\n" "cgreen" || (pprint "FAIL.\n" "cred"; exit 1)
 echo ">> INSTALLING REQUIREMENTS..."
 cd /JhonnyMusic
 pip3 install -U -r requirements.txt
